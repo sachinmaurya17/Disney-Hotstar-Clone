b26c482e4c23eef1de0be614229dd437017a6c9e
sachinmaurya17 <sachin655599@gmail.com>
2022-02-06 10:31:19 +0530

    header done

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+function home() {
+  return (
+      <Container>
+          home
+      </Container>
+  );
+}
+
+export default home;
+
+
+
+const Container = styled.div`
+
+`
---
+++ src/components/text.js
@@ -0,0 +1,22 @@
+// start
+
+// npm install  styled-component 
+
+
+
+// Part 1 :  header
+
+
+
+
+// Part 2 : slider
+
+
+
+
+
+// Part 3 :Viewer
+
+
+
+// Part 4 : Movies List
